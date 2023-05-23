26997c4e6a06c1d1c636fdb6fc7d6d4edb4cd328
dylank999 <dylank828@gmail.com>
2023-05-23 11:55:04 -0400

    Message

--- data.py
+++ data.py
@@ -31,19 +31,12 @@ WEBSITE_LIST = {"https://www.wikihow.com/":
                  "main_menu":'refresh_sens:id;header_logo',
                  "endpoints":
                  { 
-                  "random_page":  "id;nav_random",
+                  "random_page":  "id;nav_random", #Do this a bunch of times to get a new random article multiple times
                   "login_page":  "id;nav_profile",
                   "article":  'css selector;ytd-rich-item-renderer'
                  },
-                  "sub-endpoints":
-                  {
-                      "main_menu":{"trending":'relies_prev:partial link text;Trending',
-                                    
-                                   },
-                          
-                      },
-                  }
-                }
+             }
+          }
 
 WEBSITE_LIST = {"https://www.yelp.com/":
                 {"specifics":"ablock=true;",
@@ -55,19 +48,23 @@ WEBSITE_LIST = {"https://www.yelp.com/":
                  },
                   "sub-endpoints":
                   {
-                      "main_menu":{"trending":'relies_prev:partial link text;Trending',
-                                   "music":   'relies_prev:partial link text;Music',
-                                   "gaming":  'relies_prev:partial link text;Gaming',
-                                   "news":    'relies_prev:partial link text;News',
-                                   "sports":  'relies_prev:partial link text;Sports',
-                                   "learning":'relies_prev:partial link text;Learning'
-                                   },
-                      "trending":{"video":'rand_ind:css selector;ytd-video-renderer'
-                                  },
-                      "music":{"video":'rand_ind:css selector;ytd-video-renderer'
-                               },
-                      "gaming":{"video":"rand_ind:css selector;ytd-video-renderer"
-                          
+                      "main_menu":{
+                      },
+                  }
+                 }
+                }
+
+WEBSITE_LIST = {"https://www.salesforce.com/ca/":
+                {"specifics":"ablock=true;",
+                 "main_menu":'refresh_sens:id;guide-icon',
+                 "endpoints":
+                 { 
+                  "login-page":   'direct-link;signin',
+                  "video":  'css selector;ytd-rich-item-renderer'
+                 },
+                  "sub-endpoints":
+                  {
+                      "main_menu":{
                       },
                   }
                  }
